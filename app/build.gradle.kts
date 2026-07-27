e022124d9196effc6ee00eadfb3fd44204d1a4a5
DaniZakaria63 <daniootsutsuki@gmail.com>
2026-07-27 17:50:52 +0700

    CI: CI build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,14 +17,27 @@ android {
         applicationId = "com.wingsheep.freesky"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = project.findProperty("appVersionCode")?.toString()?.toInt() ?: 1
+        versionName = project.findProperty("appVersionName")?.toString() ?: "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = rootProject.file("keystore/freesky.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
+            val keystoreExists = rootProject.file("keystore/freesky.jks").exists()
+            if (keystoreExists && System.getenv("KEYSTORE_PASSWORD") != null) {
+                signingConfig = signingConfigs.getByName("release")
+            }
             optimization {
                 enable = false
             }
